e9ac535e80e5ae6c128e02670c69520872b1f97d
Alexander Upton <Alexander99W@ou.edu>
2021-02-09 18:21:39 -0600

    Add project files.

---
+++ CoinFlip/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CoinFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNbr = rand.Next(1, 3);
+
+            Console.WriteLine(randomNbr);
+
+
+        }
+    }
+}
