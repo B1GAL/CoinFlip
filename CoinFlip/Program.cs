5cc58dd2df4cb99f84d301cd043aadc79e206a31
Alexander Upton <Alexander99W@ou.edu>
2021-02-09 18:39:38 -0600

    yup

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -6,6 +6,10 @@ namespace CoinFlip
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please pick heads or tails user");
+            String 
+
+
             Random rand = new Random();
             int randomNbr = rand.Next(1, 3); //generate random number between 1 and 2
 
