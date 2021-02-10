715dd1f5e24c4193dbbcdec8c9ba7f60f07f806f
Alexander Upton <Alexander99W@ou.edu>
2021-02-09 18:25:30 -0600

    Yeah

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -7,7 +7,7 @@ namespace CoinFlip
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int randomNbr = rand.Next(1, 3);
+            int randomNbr = rand.Next(1, 3); //generate random number between 1 and 2
 
             Console.WriteLine(randomNbr);
 
