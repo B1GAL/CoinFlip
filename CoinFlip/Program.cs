5ed24276eb1a64d59394df0e4fa209eeeee41efb
Alexander Upton <Alexander99W@ou.edu>
2021-02-11 18:45:45 -0600

    Coinflip done

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -6,14 +6,50 @@ namespace CoinFlip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please pick heads or tails user");
-            String 
-
-
             Random rand = new Random();
             int randomNbr = rand.Next(1, 3); //generate random number between 1 and 2
+            int guess = 0;
+            int coin;
+            const string developer = "Alex Upton";
 
-            Console.WriteLine(randomNbr);
+            Console.WriteLine("Please pick heads or tails user");
+            string answer = Console.ReadLine();
+
+            Console.WriteLine(randomNbr); // 1=heads 2=tails
+           
+
+            if (answer == "heads")
+               
+            {
+                guess = 1;
+            }
+            else if (answer == "tails")
+            {
+                guess = 2;
+            }
+
+            if (randomNbr == 1)
+            {
+                coin = 1;
+                Console.WriteLine("Heads");
+            }
+            else
+            {
+                coin = 2;
+                Console.WriteLine("Tails");
+            }
+
+            if (guess == coin)
+            {
+                Console.WriteLine("You have guessed correctly!");
+            }
+            else
+            {
+                Console.WriteLine("You have guessed incorrectly");
+            }
+            Console.WriteLine("developer " + developer);
+
+           
 
             
 
